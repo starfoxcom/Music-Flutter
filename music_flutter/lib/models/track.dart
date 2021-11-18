18ddb369e49a7b33b471ceb94210a35543e0710f
Jorge Zamudio <alexzamudio_11@hotmail.com>
2021-11-18 16:43:56 -0600

    Create track class to handle easier the track metadata

---
+++ music_flutter/lib/models/track.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_media_metadata/flutter_media_metadata.dart';
+
+// Create track class
+class Track {
+  Metadata metadata;
+  String path;
+
+  Track(this.metadata, this.path);
+}
